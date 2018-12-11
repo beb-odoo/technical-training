6dba3e125da485402f98a82aa67adc3aabb7c130
beb-odoo <beb@odoo.com>
2018-12-11 13:40:25 +0100

    test

--- coopplanning/__manifest__.py
+++ coopplanning/__manifest__.py
@@ -25,9 +25,9 @@
     'data': [
         "data/coop_data.xml",
         "data/task_template.xml",
-        "views/config.xml",
         "views/task.xml",
         "views/task_template.xml",
+        "views/config.xml",
     ],
     # only loaded in demonstration mode
     'demo': [],
