9709bca614988c09c0bd97743df64ef415ee2d91
beb-odoo <beb@odoo.com>
2018-12-11 09:06:18 +0100

    session count

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -16,6 +16,14 @@ class Course(models.Model):
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
     level = fields.Selection([(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], string="Difficulty Level")
 
+    session_count = fields.Integer("Session Count", compute="_compute_session_count")
+
+    @api.depends('session_ids')
+    def _compute_session_count(self):
+        for course in self:
+            course.session_count = len(course.session_ids)
+                                    
+    
     # with _sql_constraints of name uniqueness, we can't use the Form->Duplicate anymore
     # hence redefine the copy function to change the name automatically when duplicating
     @api.multi
